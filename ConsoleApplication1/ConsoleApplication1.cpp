c0a186a33a05a96e8500b8ac152378b16fa12894
Serhei <s.chikita@mail.ru>
2023-11-07 11:19:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+   
+    int p; cin >> p;
+
+	switch (p)
+	{
+	case 1: 
+		cout << "^ ^\n";
+		break;
+	case 2:
+		cout << "^\n";
+		break;
+	case 3:
+		cout << "Norm\n";
+		break;
+	case 4:
+		cout << "v\n";
+		break;
+	case 5:
+		cout << "v v\n";
+		break;
+	default: "Вы проверяете не давление\n";
+		break;
+	}
+
+
+    return 0;
+}
---
+++ SwitchCase/SwitchCase.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int fact(int n) {
+	if (n <= 1)
+	{
+		return 1;
+	}
+
+	return n * fact(n - 1);
+}
+
+int main() {
+    
+	int a = 5;
+
+	cout << fact(a) << endl;
+   
+
+    return 0;
+}
